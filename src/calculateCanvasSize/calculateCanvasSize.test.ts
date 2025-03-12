2fc4f2b3fae2d77e172ce0d15632a50effabae99
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-12 14:32:58 -0400

    Refactored test to use 'it' block for consistency and improved readability

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -1,9 +1,7 @@
 import calculateCanvasSize from "./calculateCanvasSize";
 
 describe("Unit Tests - calculateCanvasSize", () => {
-  test("returns correct result", () => {
-    const result = calculateCanvasSize("10", "100");
-
-    expect(result).toEqual(1000);
-  })
+  it("Verify that the function correctly multiplies two valid integer strings", () => {
+    expect(calculateCanvasSize("10", "100")).toBe(1000);
+  });
 });
