acf8cf22850ff39096e605a8132d454b1c1007a2
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-12 14:41:35 -0400

    Added test to verify correct canvas size calculation when the width is negative.

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -7,4 +7,7 @@ describe("Unit Tests - calculateCanvasSize", () => {
   it("Verify that the function correctly calculates the canvas size when the length is negative", () => {
     expect(calculateCanvasSize("-5", "10")).toBe(-50);
   });
+  it("Verify that the function correctly calculates the canvas size when the width is negative", () => {
+    expect(calculateCanvasSize("10", "-2")).toBe(-20);
+  });
 });
