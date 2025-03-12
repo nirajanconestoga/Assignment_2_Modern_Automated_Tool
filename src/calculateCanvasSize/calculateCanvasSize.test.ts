21ae523b77ac0986be734e1e151ad0cf154c8726
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-03-12 14:39:49 -0400

    Added test to verify correct canvas size calculation when the length is negative.

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -4,4 +4,7 @@ describe("Unit Tests - calculateCanvasSize", () => {
   it("Verify that the function correctly multiplies two valid integer strings", () => {
     expect(calculateCanvasSize("10", "100")).toBe(1000);
   });
+  it("Verify that the function correctly calculates the canvas size when the length is negative", () => {
+    expect(calculateCanvasSize("-5", "10")).toBe(-50);
+  });
 });
